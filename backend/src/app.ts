727b669a17f6baf93684c5926f53887bb84007b5
gabrielDev <gabrielrpdev2@gmail.com>
2025-07-07 14:29:29 -0400

    feat: definida la ruta de auth-login

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import {config} from "dotenv";
+import authRoutes from './routes/authRoutes'
 
 config()
 const app = express()
@@ -7,6 +8,6 @@ const app = express()
 app.use(express.json()) //para poner el cuerpo json
 
 // declarar las rutas:   
-app.use('name_route',() => {})
+app.use('/auth',authRoutes)
 
 export default app
