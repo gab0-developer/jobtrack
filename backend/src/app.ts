e00d0650ff0f6f43a1a249f573b80366839b5588
gabrielDev <gabrielrpdev2@gmail.com>
2025-07-13 18:14:50 -0400

    feat: agregado cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,18 @@
 import express from "express";
+import cors from 'cors';
 import {config} from "dotenv";
 import authRoutes from './routes/authRoutes'
 import jobRoutes from './routes/jobRoutes'
 
 config()
 const app = express()
+// O configuración personalizada (recomendado para producción)
+app.use(cors({
+  origin: 'http://localhost:3000', // Reemplaza con el origen de tu app React
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true // Si usas cookies o autenticación JWT en headers
+}));
 
 app.use(express.json()) //para poner el cuerpo json
 
