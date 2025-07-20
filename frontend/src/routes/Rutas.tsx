c668bce4877f7560ea3c2087e8167e61c563fcfd
gabrielDev <gabrielrpdev2@gmail.com>
2025-07-19 23:40:19 -0400

    feat: creada validacion de rutas para usuario autenticado

--- frontend/src/routes/Rutas.tsx
+++ frontend/src/routes/Rutas.tsx
@@ -1,27 +1,25 @@
-import { Route, Routes } from 'react-router-dom'
-import Register from '../pages/auth/Register'
-import Login from '../pages/auth/Login'
-// import Dashboard from '../pages/Dashboard'
-// import { ProtectedRouters } from './ProtectedRouters'
+// Rutas.tsx
+import { Route, Routes } from 'react-router-dom';
+import { AuthProvider } from '../context/AuthContext';
+import { ProtectedRouters } from './ProtectedRouters';
+import Dashboard from '../pages/Dashboard';
+import Register from '../pages/auth/Register';
+import Login from '../pages/auth/Login';
 
-type Props = {}
-
-const Rutas = ({}: Props) => {
+const Rutas = () => {
   return (
-    <>
-        <Routes>
-            <Route index element={ <Register/> } /> 
-            <Route path='/login' element={ <Login/> }/> 
-
-            {/* ruta para acceder solo si tiene usuario */}
-            {/* <Route element={< ProtectedRouters user={storedUser}/>}>
-              <Route path='/dashboard' element={< Dashboard user={storedUser}/>} />
-             
-            </Route> */}
-        </Routes>        
-    </>
-  )
-}
+    <AuthProvider>
+      <Routes>
+        <Route index element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        
+        <Route element={<ProtectedRouters />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+      </Routes>
+    </AuthProvider>
+  );
+};
 
 
 export default Rutas
