6d1daebfcf9abe5b97660d419b6a6278d6e0c7f0
gabrielDev <gabrielrpdev2@gmail.com>
2025-07-24 21:53:32 -0400

    feat: agregado tastify en el codigo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,11 +5,15 @@ import './App.css'
 import { BrowserRouter  as Router} from 'react-router-dom'
 import Rutas from './routes/Rutas'
 
+import { ToastContainer, toast } from 'react-toastify';
+
+
 function App() {
 
   return (
     <>
       <Router>
+        <ToastContainer />
         <Rutas />
       </Router>
       {/* <Register /> */}
