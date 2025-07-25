30bd0687192b7506a24710f8e43359d914ee5504
gabrielDev <gabrielrpdev2@gmail.com>
2025-07-24 22:01:58 -0400

    feat: crada funciones de crud e implementado modales

--- frontend/src/components/Tarjetas.tsx
+++ frontend/src/components/Tarjetas.tsx
@@ -1,4 +1,3 @@
-
 import { format, parseISO } from 'date-fns';
 import { es } from 'date-fns/locale';
 // CARDS
@@ -9,15 +8,17 @@ import Avatar from '@mui/material/Avatar';
 import IconButton, { type IconButtonProps } from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import { red } from '@mui/material/colors';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { CardContent } from '@mui/material';
 
+// ICONS 
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 
 interface ExpandMoreProps extends IconButtonProps {
   expand: boolean;
 }
+
+
 
 const ExpandMore = styled((props: ExpandMoreProps) => {
   const { expand, ...other } = props;
@@ -49,12 +50,14 @@ type Props = {
     job_title:string,
     created_at:string,
     company:string,
-
+    status:string,
+    handleOpenDialogDelete:(index:number) => void, 
+    handleOpenDialogUpdate:(index:number) => void, 
 }
 
 
 
-const Tarjetas = ({index,primeraLetra,job_title,created_at,company}: Props) => {
+const Tarjetas = ({index,primeraLetra,job_title,created_at,company,status,handleOpenDialogDelete,handleOpenDialogUpdate}: Props) => {
 
     const ExpandMore = styled((props: ExpandMoreProps) => {
     const { expand, ...other } = props;
@@ -80,10 +83,11 @@ const Tarjetas = ({index,primeraLetra,job_title,created_at,company}: Props) => {
     ],
     }));
 
+    
+
     return (
         <>
-        
-            <Card sx={{ maxWidth: 345 }} key={index}>
+            <Card sx={{ maxWidth: 345, my:1 }} key={index}>
                 <CardHeader
                     avatar={
                     <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
@@ -93,10 +97,14 @@ const Tarjetas = ({index,primeraLetra,job_title,created_at,company}: Props) => {
                     action={
                       <>
                         <IconButton aria-label="settings" >
-                            <EditIcon color="primary" onClick = {() =>{console.log('editar')}} />
+                            <EditIcon color="primary" 
+                              onClick = {() => handleOpenDialogUpdate(index)}
+                            />
                         </IconButton>
                         <IconButton aria-label="settings" >
-                            <DeleteIcon sx={{ color: red[500] }} onClick = {() =>{console.log('delete')}} />
+                            <DeleteIcon sx={{ color: red[500] }} 
+                              onClick={() => handleOpenDialogDelete(index)} 
+                            />
                         </IconButton>
                       </>
                     }
@@ -106,6 +114,9 @@ const Tarjetas = ({index,primeraLetra,job_title,created_at,company}: Props) => {
                 <CardContent>
                     <Typography variant="body2" sx={{ color: 'text.secondary' }}>
                         <strong>Compañia o empresa aplicada: </strong>{company}
+                    </Typography>
+                    <Typography variant="caption" gutterBottom>
+                        <strong>Estatus: </strong>{status}
                     </Typography>
                 </CardContent>
             </Card>
--- frontend/src/pages/job/index.tsx
+++ frontend/src/pages/job/index.tsx
@@ -1,14 +1,25 @@
+import * as React from 'react';
+
 import RegisterJob from "./RegisterJob"
 
-
-import { Box } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { Box, Typography } from "@mui/material";
 import { useAuth } from "../../context/AuthContext";
 import { regexPrimeraLetra } from "../../utils/regexPatterns";
-import axios from "axios";
-import { API_URL } from "../../api/application";
 import { useEffect, useState } from "react";
 import Tarjetas from "../../components/Tarjetas";
+// SLIDE ALERT DIALOG
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Slide from '@mui/material/Slide';
+import type { TransitionProps } from '@mui/material/transitions';
 
+import { DeleteAxios, getAxios, getIDAxios } from '../../hooks/axiosApi';
+import UpdateJob from './UpdateJob';
 
 // En Listjob.tsx
 interface Postulation {
@@ -16,94 +27,154 @@ interface Postulation {
   job_title: string;
   created_at:string
   company: string;
+  status_id: string;
 }
 
 
 type Props = {
     Postulation:string[]
+    
 }
 
+const Transition = React.forwardRef(function Transition(
+  props: TransitionProps & {
+    children: React.ReactElement<any, any>;
+  },
+  ref: React.Ref<unknown>,
+) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
 const index = ({}: Props) => {
+    
 
     const { user } = useAuth();
 
+    const navigate = useNavigate();
     const [postulations, setPostulations] = useState<Postulation[]>([]);
+    const [selectedPostulation, setSelectedPostulation] = useState<Postulation | null>(null);
 
     const regexmatch = user.name.match(regexPrimeraLetra);
     const primeraLetra = regexmatch?.groups?.inicial || "";
 
-     
+    const [openDialogDelete, setOpenDialogDelete] = React.useState(false);
+    const [openDialogUpdate, setOpenDialogUpdate] = React.useState(false);
+    const [postulationID, setPostulationID] = useState<number>()
 
-    const getAxios = async () =>{
-        const url = `${API_URL}/job/`
-    
-        try {
-            const resp = await axios.get(url,{
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${localStorage.getItem('authToken')}` // Opcional
-                }
-            });
-            console.log('Get tareas usuario',resp)
-    
-            if (resp.status === 200 && resp.statusText === "OK") {
-                const resp_data = await resp.data
-                if (resp_data.success) {
-                    setPostulations(resp_data.data)
-                    console.log('resp_data',resp_data.data)
-                }
-                // console.log(resp_data.message)
-                
-            }else{
-                console.log(`Ha ocurrido un error inesperado`)
-                return 
-            }
-    
-        
-        
-        } catch (error) {
-            // if (axios.isAxiosError(error)) {
-            // // Manejo específico de errores de Axios
-            //     if (error.response) {
-            //         const dataError = error.response
-            //         if (dataError.data.success === false) {
-                        
-            //             console.log(dataError.data.message)
-            //         }
-            //         if (dataError.data.status === 401) {
-            //             // Manejo de token expirado
-            //             navigate('/login');
-            //         }
-            //     }
-            // }
-        }
-    }
+     // Función para refrescar las postulaciones
+    const refreshPostulations =  async () => {
+        // await getAxios(); // Reutiliza tu función existente
+        await getAxios(`/job/`,setPostulations,navigate)
+    };
 
-    // Función para refrescar las postulaciones
-    const refreshPostulations = async () => {
-        await getAxios(); // Reutiliza tu función existente
+
+
+    const deletePostulation = () => {
+        console.log('postulationID: ',postulationID)
+        DeleteAxios(`/job/${postulationID}`,navigate,refreshPostulations)
+        setOpenDialogDelete(false);
     };
     
-    useEffect(() =>{
-        getAxios()
-    },[])
+    const handleOpenDialogUpdate = (index:number) => {
+        console.log(`Dialog ID: ${index}`)
+        getIDAxios<Postulation>(`/job/${index}`,setSelectedPostulation)
+        setOpenDialogUpdate(true);
+    };
+    const handleOpenDialogDelete = (index:number) => {
+        console.log(`Dialog ID: ${index}`)
+        setPostulationID(index)
+        setOpenDialogDelete(true);
+    };
+    const handleCloseDialogUpdate = () => {
+      setOpenDialogUpdate(false);
+    };
+    const handleCloseDialog = () => {
+      setOpenDialogDelete(false);
+    };
+    
 
+    useEffect(() =>{
+        getAxios(`/job/`,setPostulations,navigate)
+    },[])
 
     return (
         <>
+
+            <React.Fragment>
+                <Dialog
+                    open={openDialogDelete}
+                    slots={{
+                        transition: Transition,
+                    }}
+                    keepMounted
+                    onClose={handleCloseDialog}
+                    aria-describedby="alert-dialog-slide-description"
+                    >
+                    <DialogTitle>{"Estas seguro de eliminación?"}</DialogTitle>
+                    <DialogContent>
+                        <DialogContentText id="alert-dialog-slide-description">
+                        Una vez eliminado este registro, no se podra recurpar.
+                        </DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button variant='contained' color='error' onClick={handleCloseDialog}>cancelar</Button>
+                        <Button variant='contained' color='success' onClick={() => deletePostulation()}>Eliminar</Button>
+                    </DialogActions>
+                </Dialog>
+                <Dialog
+                    open={openDialogUpdate}
+                    slots={{
+                        transition: Transition,
+                    }}
+                    keepMounted
+                    onClose={handleCloseDialogUpdate}
+                    aria-describedby="alert-dialog-slide-description"
+                    >
+                    <DialogTitle>{"Estas seguro de eliminación?"}</DialogTitle>
+                    <DialogContent>
+                        <DialogContentText id="alert-dialog-slide-description">
+                             {selectedPostulation ? (
+                                // <p key={selectedPostulation.id}>{selectedPostulation.id}</p>
+                                <UpdateJob
+                                    id={selectedPostulation.id}
+                                    job_title={selectedPostulation.job_title}
+                                    position={selectedPostulation.position}
+                                    link={selectedPostulation.link}
+                                    created_at={selectedPostulation.created_at}
+                                    company={selectedPostulation.company}
+                                    status={selectedPostulation.status_id}
+                                    refreshPostulations={refreshPostulations}
+                                    handleCloseDialogUpdate={handleCloseDialogUpdate}
+                                />
+                                ) : (
+                                <Typography>Cargando...</Typography>
+                                )}
+                            
+                        </DialogContentText>
+                    </DialogContent>
+                    {/* <DialogActions>
+                        <Button variant='contained' color='error' onClick={handleCloseDialogUpdate}>cancelar</Button>
+                        <Button variant='contained' color='success' onClick={() => deletePostulation()}>Eliminar</Button>
+                    </DialogActions> */}
+                </Dialog>
+            </React.Fragment>
+            
+        
             
            <RegisterJob
                 onPostulationCreated={refreshPostulations}
              />
            
             <Box component='div' sx={{display:'flex',alignItems:'center',justifyContent:'space-around',flexWrap:'wrap',marginY:2}}>
-                {postulations.map((postulation,index) => (
+                {postulations.map((postulation) => (
                     <Tarjetas 
-                        index={index}
+                        index={postulation.id}
                         primeraLetra={primeraLetra}
                         job_title={postulation.job_title}
                         created_at={postulation.created_at}
                         company={postulation.company}
+                        status={postulation.status_id}
+                        handleOpenDialogDelete={handleOpenDialogDelete}
+                        handleOpenDialogUpdate={handleOpenDialogUpdate}
                     />
                 ))}
             </Box>
