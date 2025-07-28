93d3751368fbfb729baf8760154d9c08e1d88c0f
gabrielDev <gabrielrpdev2@gmail.com>
2025-07-27 21:51:26 -0400

    feat: Agregado tarjetas para mostrar informacion detallada

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,27 +1,89 @@
-import { useAuth } from '../context/AuthContext';
-import { Button } from '@mui/material';
+import { useEffect, useState } from 'react';
+import Cards from '../components/Cards';
+import { getAxios } from '../hooks/axiosApi';
+import { useNavigate } from 'react-router-dom';
+import { Box,Container,Typography } from '@mui/material';
 
+import AssignmentTurnedInIcon from '@mui/icons-material/AssignmentTurnedIn';
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import InterpreterModeIcon from '@mui/icons-material/InterpreterMode';
 
+interface Postulation {
+  id: number;
+  job_title: string;
+  position: string;
+  link: string;
+  created_at:string
+  company: string;
+  status_id: string;
+}
 
 type Props = {}
 
 const Dashboard = ({}:Props) => {
+  const navigate = useNavigate();
 
-  const { user,logout } = useAuth();
+  const [postulations, setPostulations] = useState<Postulation[]>([]);
+  const [postulationPendiente, setPostulationPendiente] = useState<Postulation[]>([]);
+  const [postulationAprobada, setPostulationAprobada] = useState<Postulation[]>([]);
+  const [postulationRechazada, setPostulationRechazada] = useState<Postulation[]>([]);
+  const [postulationEntrevista, setPostulationEntrevista] = useState<Postulation[]>([]);
+  // Función para refrescar las postulaciones
+  const refreshPostulations =  async () => {
+      await getAxios(`/job/`,setPostulations,navigate)
+  };
+
+  const findPostulactionStatus = () =>{
+    setPostulationPendiente(postulations.filter((item) => item.status_id == `1`))
+    setPostulationAprobada(postulations.filter((item) => item.status_id == `2`))
+    setPostulationRechazada(postulations.filter((item) => item.status_id == `3`))
+    setPostulationEntrevista(postulations.filter((item) => item.status_id == `4`))
+
+  }
+  
+  useEffect(() =>{
+    refreshPostulations()
+  },[])
+  useEffect(() =>{
+    findPostulactionStatus()
+  },[postulations])
 
 
   return (
     <>
-      <h1>Bienvenido, {user?.name}</h1>
-      <Button 
-        variant="contained" 
-        color="error"
-        onClick={logout}
-      >
-        Cerrar sesión
-      </Button>
-    
+      <Container maxWidth="xl">
+        <Box component='div'>
+          <Typography variant='h4' >
+            Postulaciones:
+          </Typography>
 
+        </Box>
+        <Box component='div' sx={{display:'flex',alignItems:'center',justifyContent:'space-around',flexWrap:'wrap'}}>
+          <Cards 
+            title='Aprobadas'
+            count={postulationAprobada.length}
+            icon={<AssignmentTurnedInIcon fontSize="large" color="success" />}
+          />
+          <Cards 
+            title='Pendientes'
+            count={postulationPendiente.length}
+            icon={<AccessTimeIcon fontSize="large" color="warning" />}
+          />
+          <Cards 
+            title='Rechazadas'
+            count={postulationRechazada.length}
+            icon={<HighlightOffIcon fontSize="large" color="error" />}
+          />
+          <Cards 
+            title='En entrevistas'
+            count={postulationEntrevista.length}
+            icon={<InterpreterModeIcon fontSize="large" color="secondary" />}
+          />
+        </Box>
+      </Container>
+    
+      
     </>
   )
 }
