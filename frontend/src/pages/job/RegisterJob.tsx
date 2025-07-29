9dca0a305317d141fc4238e65713266e7393b843
gabrielDev <gabrielrpdev2@gmail.com>
2025-07-28 22:41:04 -0400

    feat: implementado reac-hook-form

--- frontend/src/pages/job/RegisterJob.tsx
+++ frontend/src/pages/job/RegisterJob.tsx
@@ -3,58 +3,115 @@ import { Box, Button, TextField, Dialog, DialogTitle, DialogContent, DialogActio
 import { useNavigate } from 'react-router-dom';
 import { postAxios } from "../../hooks/axiosApi";
 
+import { useForm, type SubmitHandler } from "react-hook-form";
+
+
 type Props = {
   refreshPostulations: () => void;
 };
 
+type FormValues = {
+  job_title: string;
+  company: string;
+  position: string;
+  link: string;
+};
+
+
 const RegisterJob = ({refreshPostulations}: Props) => {
   
   const navigate = useNavigate();
-
-
   const [open, setOpen] = useState<boolean>(false);
 
-  const [jobtitle,setJobtitle] = useState<string>('')
-  const [company,setCompany] = useState<string>('')
-  const [position,setPosition] = useState<string>('')
-  const [link,setLink] = useState<string>('')
-  
-  const obj_data = {
-    status_id:1,
-    job_title: jobtitle,
-    company,
-    position,
-    link,
-    token: localStorage.getItem('authToken')
-  }
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors }
+  } = useForm<FormValues>();
 
-  const handleRegister = (e:any) =>{
-    e.preventDefault()
-    postAxios(`/job/`,obj_data,navigate,refreshPostulations)
-    setOpen(false); 
-  }
+
+  const onSubmit: SubmitHandler<FormValues> = (data) => {
+    const obj_data = {
+      ...data,
+      status_id: 1,
+      // token: localStorage.getItem("authToken"),
+    };
+
+    postAxios(`/job/`, obj_data, navigate, refreshPostulations);
+    reset();
+    setOpen(false);
+  };
 
   return (
     <>
     
-      <Button variant="outlined" onClick={() => setOpen(true)}>
+      <Button variant="contained" color="info" onClick={() => setOpen(true)}>
         Registrar postulación
       </Button>
 
       <Dialog open={open} onClose={() => setOpen(false)}>
         <DialogTitle>Registrar postulación profesional</DialogTitle>
         <DialogContent>
-          <Box component="form" onSubmit={handleRegister} sx={{ display: 'flex', flexDirection: 'column', gap: 2, mt: 2 }}>
-            <TextField label="Título" value={jobtitle} onChange={(e) => setJobtitle(e.target.value)} variant="standard" />
-            <TextField label="Compañía/empresa" value={company} onChange={(e) => setCompany(e.target.value)} variant="standard" />
-            <TextField label="Puesto profesional" value={position} onChange={(e) => setPosition(e.target.value)} variant="standard" />
-            <TextField label="Link de postulación" value={link} onChange={(e) => setLink(e.target.value)} variant="standard" />
+          <Box component="form" onSubmit={handleSubmit(onSubmit)} sx={{ display: 'flex', flexDirection: 'column', gap: 2, mt: 2 }}>
+            <TextField
+              label="Título"
+              variant="standard"
+              {...register("job_title", { 
+                required: "Ingresar el titulo",
+                pattern: {
+                  value: /^[A-Za-zÁÉÍÓÚáéíóúñÑ\s]+$/,
+                  message: "Solo se permiten letras y espacios"
+                }
+              })}
+              error={!!errors.job_title}
+              helperText={errors.job_title?.message}
+            />
+            <TextField
+              label="Compañía/empresa"
+              variant="standard"
+              {...register("company", { 
+                required: "Ingresar la empresa",
+                pattern: {
+                  value: /^[A-Za-z0-9ÁÉÍÓÚáéíóúñÑ\s]+$/,
+                  message: "Solo letras, números y espacios"
+                }
+              })}
+              error={!!errors.company}
+              helperText={errors.company?.message}
+            />
+            <TextField
+              label="Puesto profesional"
+              variant="standard"
+              {...register("position", { 
+                required: "Cargo profesional postulado",
+                pattern: {
+                  value: /^[A-Za-zÁÉÍÓÚáéíóúñÑ\s]+$/,
+                  message: "Solo se permiten letras y espacios"
+                }
+              })}
+              error={!!errors.position}
+              helperText={errors.position?.message}
+            />
+            <TextField
+              label="Link de postulación"
+              variant="standard"
+              {...register("link", {
+                required: "Ingresar link donde postulaste",
+                pattern: {
+                  value: /^(https?:\/\/)?([\w\-]+\.)+[\w\-]+(\/[\w\-._~:/?#[\]@!$&'()*+,;=]*)?$/,
+                  message: "URL inválida"
+                }
+              })}
+              error={!!errors.link}
+              helperText={errors.link?.message}
+            />
+            <DialogActions>
+              <Button onClick={() => setOpen(false)} color="error">Cancelar</Button>
+              <Button type="submit" color="success">Guardar</Button>
+            </DialogActions>
           </Box>
         </DialogContent>
-        <DialogActions>
-          <Button onClick={() => setOpen(false)} color="error">Cancelar</Button>
-          <Button type="submit" onClick={handleRegister} color="primary">Guardar</Button>
-        </DialogActions>
       </Dialog>
 
     </>
--- frontend/src/pages/job/UpdateJob.tsx
+++ frontend/src/pages/job/UpdateJob.tsx
@@ -1,182 +1,200 @@
-import { useState, useEffect } from "react";
-import { Autocomplete, Box, Button , TextField } from '@mui/material';
-
-import { format, parseISO } from 'date-fns';
-// import { es } from 'date-fns/locale';
-
-import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import {
+  Autocomplete,
+  Box,
+  Button,
+  TextField
+} from "@mui/material";
+import { format, parseISO } from "date-fns";
+import { useNavigate } from "react-router-dom";
 import { getAxios, putAxios } from "../../hooks/axiosApi";
+import { useForm, Controller } from "react-hook-form";
 
 interface StatusItem {
   id: number;
   status_name: string;
-} 
+}
 
 type Props = {
-    id:number,
-    job_title:string,
-    position:string,
-    link:string,
-    created_at:string,
-    company:string,
-    status:string,
-    refreshPostulations: () => void;
-    handleCloseDialogUpdate: () => void;
+  id: number;
+  job_title: string;
+  position: string;
+  link: string;
+  created_at: string;
+  company: string;
+  status: string;
+  refreshPostulations: () => void;
+  handleCloseDialogUpdate: () => void;
+};
+
+type FormData = {
+  job_title: string;
+  position: string;
+  link: string;
+  company: string;
+  status_id: StatusItem | null;
 };
 
 const UpdateJob = ({
-    id,
-    job_title,
-    position:propPosition,
-    link:propLink,
-    created_at,
-    company:propCompany,
-    status:propStatus,
-    refreshPostulations,
-    handleCloseDialogUpdate,
-    
-    }: Props) => {
-  
+  id,
+  job_title,
+  position,
+  link,
+  created_at,
+  company,
+  status,
+  refreshPostulations,
+  handleCloseDialogUpdate
+}: Props) => {
   const navigate = useNavigate();
 
-
-  const [jobTitle, setJobTitle] = useState<string>(job_title || '');
-  const [localPosition,setlocalPosition] = useState<string>(propPosition || '')
-  const [localLink,setLocalLink] = useState<string>(propLink || '')
-  const [localCompany, setLocalCompany] = useState<string>(propCompany || '');
-  const [listStatus, setListStatus] = useState<StatusItem[]>([]);
-  const [selectedStatus, setSelectedStatus] = useState<StatusItem | null>(null);
-
-//   const url = `${API_URL}/job/${id}`
-  const obj_data = {
-    status_id:selectedStatus?.id,
-    job_title: jobTitle,
-    company:localCompany,
-    position:localPosition,
-    link:localLink,
-    // id:id,
-  }
-
-  const handleUpdate = (e:any) =>{
-    e.preventDefault()
-    putAxios(`/job/${id}`,obj_data,navigate,refreshPostulations)
-    handleCloseDialogUpdate()
-    console.log('obj_data',obj_data)
-  }
-
-  useEffect(() =>{
-      getAxios(`/status/`,setListStatus,navigate)
-  },[])
-
- // indicar estado actual cuando la lista esté cargada
-  useEffect(() => {
-    if (listStatus.length > 0 && propStatus) {
-      // Convertir propStatus a número si es necesario
-      const statusId = typeof propStatus === 'string' ? parseInt(propStatus) : propStatus;
-      const currentStatus = listStatus.find(item => item.id === statusId);
-      setSelectedStatus(currentStatus || null);
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    control,
+    formState: { errors }
+  } = useForm<FormData>({
+    defaultValues: {
+      job_title,
+      position,
+      link,
+      company,
+      status_id: null
     }
-  }, [listStatus, propStatus]);
+  });
 
+  const [listStatus, setListStatus] = useState<StatusItem[]>([]);
 
+  useEffect(() => {
+    getAxios(`/status/`, setListStatus, navigate);
+  }, []);
+
+  useEffect(() => {
+    if (listStatus.length > 0 && status) {
+      const statusId = typeof status === "string" ? parseInt(status) : status;
+      const currentStatus = listStatus.find((item) => item.id === statusId);
+      setValue("status_id", currentStatus || null);
+    }
+  }, [listStatus, status, setValue]);
+
+  const onSubmit = (data: FormData) => {
+    const obj_data = {
+      job_title: data.job_title,
+      company: data.company,
+      position: data.position,
+      link: data.link,
+      status_id: data.status_id?.id
+    };
+
+    putAxios(`/job/${id}`, obj_data, navigate, refreshPostulations);
+    handleCloseDialogUpdate();
+  };
 
   return (
-    <>
-        <Box component='div' key={id}>
-            <Box component='form' className="form" onSubmit={handleUpdate}>
-                <TextField
-                fullWidth
-                type="text"
-                id="input"
-                label="titulo"
-                value={jobTitle}    
-                onChange={(e) => {setJobTitle(e.target.value)}}
-                variant="standard"
-            
-                />
-                <TextField
-                    fullWidth
-                    type="text"
-                    id="input"
-                    label="Compañia/empresa"
-                    value={localCompany}    
-                    onChange={(e) => {setLocalCompany(e.target.value)}}
-                    variant="standard"
-                
-                />
-                
-                <TextField
-                    fullWidth
-                    type="text"
-                    id="input"
-                    label="Puesto profesional"
-                    value={localPosition}    
-                    onChange={(e) => {setlocalPosition(e.target.value)}}
-                    variant="standard"
-                
-                />
-                
-                <TextField
-                    
-                    type="text"
-                    id="input"
-                    label="link de postulación"
-                    value={localLink}    
-                    onChange={(e) => {setLocalLink(e.target.value)}}
-                    variant="standard"
-                
-                />
-                <TextField
-                    
-                    type="date"
-                    id="input"
-                    label="Fecha de postulación"
-                    value={format(parseISO(created_at), "yyyy-MM-dd")}    
-                    variant="filled"
-                    InputProps={{
-                      readOnly: true, // Bloquea cambios pero mantiene el estilo normal
-                    }}
-                />
+    <Box component="div" key={id}>
+      <Box component="form" onSubmit={handleSubmit(onSubmit)} className="form">
+        <TextField
+          fullWidth
+          label="Título"
+          variant="standard"
+          {...register("job_title", {
+            required: "El título es obligatorio",
+            pattern: {
+              value: /^[A-Za-zÁÉÍÓÚáéíóúñÑ\s]+$/,
+              message: "Solo letras y espacios"
+            }
+          })}
+          error={!!errors.job_title}
+          helperText={errors.job_title?.message}
+        />
 
-                <Autocomplete 
-                        id="autocomplete" 
-                        options={listStatus}
-                        value={selectedStatus}
-                        onChange={(e, newValue: StatusItem | null) => {
-                          setSelectedStatus(newValue);
-                        }}
-                        getOptionLabel={(option) => option.status_name}
-                        renderInput={(params) => <TextField {...params} label="Estatus" variant="outlined" />}
-                      />
-{/* 
-                 <Autocomplete
-                    id="status-select"
-                    options={listStatus}
-                    value={selectedStatus}
-                    onChange={(_, newValue) => setSelectedStatus(newValue)}
-                    getOptionLabel={(option) => option.status_name}
-                    isOptionEqualToValue={(option, value) => option.id === value.id}
-                    renderInput={(params) => (
-                      <TextField
-                        {...params}
-                        label="Estado actual"
-                        variant="outlined"
-                        fullWidth
-                        
-                      />
-                    )}
-                  /> */}
+        <TextField
+          fullWidth
+          label="Compañía/empresa"
+          variant="standard"
+          {...register("company", {
+            required: "La compañía es obligatoria",
+            pattern: {
+              value: /^[A-Za-z0-9ÁÉÍÓÚáéíóúñÑ\s]+$/,
+              message: "Solo letras, números y espacios"
+            }
+          })}
+          error={!!errors.company}
+          helperText={errors.company?.message}
+        />
 
-                
-                <Box component='div' sx={{mt:1}}>
-                    <Button type="submit" id="sumit" variant="contained">Guardar</Button>
-                </Box>
+        <TextField
+          fullWidth
+          label="Puesto profesional"
+          variant="standard"
+          {...register("position", {
+            required: "El puesto es obligatorio",
+            pattern: {
+              value: /^[A-Za-zÁÉÍÓÚáéíóúñÑ\s]+$/,
+              message: "Solo letras y espacios"
+            }
+          })}
+          error={!!errors.position}
+          helperText={errors.position?.message}
+        />
 
-            </Box>
+        <TextField
+          fullWidth
+          label="Link de postulación"
+          variant="standard"
+          {...register("link", {
+            required: "El enlace es obligatorio",
+            pattern: {
+              value: /^(https?:\/\/)?([\w\-]+\.)+[\w\-]+(\/[\w\-._~:/?#[\]@!$&'()*+,;=]*)?$/,
+              message: "URL inválida"
+            }
+          })}
+          error={!!errors.link}
+          helperText={errors.link?.message}
+        />
+
+        <TextField
+          fullWidth
+          label="Fecha de postulación"
+          variant="filled"
+          value={format(parseISO(created_at), "yyyy-MM-dd")}
+          InputProps={{ readOnly: true }}
+        />
+
+        {/* Integración del Autocomplete con React Hook Form usando Controller. */}
+        <Controller
+          name="status_id"
+          control={control}
+          rules={{ required: "Debes seleccionar un estado" }}
+          render={({ field }) => (
+            <Autocomplete
+              {...field}
+              options={listStatus}
+              getOptionLabel={(option) => option?.status_name || ""}
+              isOptionEqualToValue={(option, value) => option.id === value.id}
+              onChange={(_, data) => field.onChange(data)}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  label="Estatus"
+                  variant="outlined"
+                  error={!!errors.status_id}
+                  helperText={errors.status_id?.message}
+                />
+              )}
+            />
+          )}
+        />
+
+        <Box sx={{ mt: 1 }}>
+          <Button type="submit" variant="contained">
+            Guardar
+          </Button>
         </Box>
+      </Box>
+    </Box>
+  );
+};
 
-    </>
-  )
-}
-
-export default UpdateJob
+export default UpdateJob;
